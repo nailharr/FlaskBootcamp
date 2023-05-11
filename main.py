c8bea172a66852802746e5812e103ee757e8d41a
nailharr <nailharr@gmail.com>
2023-05-12 00:43:33 +0300

    csv to html

--- main.py
+++ main.py
@@ -3,26 +3,31 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def main():
-#     return "<h1>Hello, world!</h1><br><a href='/index'>2st page</a>"\
-
 @app.route('/')
 def main():
     variable = "placeholder was here"
     return render_template("main_page.html", placeholder=variable)
 
 
-@app.route('/calc/<x>/<y>')
-def calc(x, y):
-    return f"<h2>Result:</h2> {int(x) + int(y)} <br><a href='/'>Go to main page</a>"
+@app.route('/plus/<x>/<y>')
+def plus(x, y):
+    return f'<h2>Result:</h2> {int(x) + int(y)} <br><br>' \
+           f'<button type="button">' \
+           f'<a href="/">Go to main page</a>' \
+           f'</button>'
 
-# TODO: add table parsing
+
 @app.route('/table')
 def table():
     with open("files_in/peoples_data.csv", "r", encoding="utf8") as file:
-        print(file.read())
-    return render_template("table_peoples.html")
+
+        people_data = list()
+        for line in file.readlines():
+            people_data.append(tuple(line.split('\n')[0].split(',')))
+
+        print(people_data)
+
+    return render_template("table_peoples.html", data=people_data)
 
 if __name__ == '__main__':
     app.run()
